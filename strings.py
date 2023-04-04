88accafcc7a154d4942fc8d234d3bace7e866df7
Carina Ogbat <carinawrightogbat@gmail.com>
2023-04-03 20:03:44 -0700

    added negative indices

--- strings.py
+++ strings.py
@@ -5,3 +5,11 @@ def password_generator(first_name, last_name):
 
 temp_password = password_generator(first_name, last_name)
 print(temp_password) ##logs ikouki
+
+#negative indices
+company_motto = "Copeland's Corporate Company helps you capably cope with the constant cacophony of daily life"
+second_to_last = company_motto[-2]
+final_word = company_motto[-4:]
+print(second_to_last) #prints f
+print(final_word) #prints life
+
