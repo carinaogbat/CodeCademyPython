c9cb1d506ccfcabf944596c2bbf5568f77fd2f37
Carina Ogbat <carinawrightogbat@gmail.com>
2023-04-05 10:27:56 -0700

    added poem example

--- strings.py
+++ strings.py
@@ -74,10 +74,18 @@ def password_generator(user_name):
   return password
 
 #STRING METHODS:
-# .lower() - all lowercase
-# .upper() - all uppercase
-# .title() - capitalizes all first letters
-# .split() - splits at spaces as default, otherwise by delimeter .split('delimeter')
+# str.lower() - all lowercase
+# str.upper() - all uppercase
+# str.title() - capitalizes all first letters
+# str.split() - splits at spaces as default, otherwise by delimeter var.split('delimeter')
+# delimeter.join(str) - ' '.join(str) - joins at delimeter delimeter.join(str)
+# str.strip() - removes all whitespace characters from beginning to end, you can also strip
+    #with a character argument str.strip('!') *note this will only strip argument and not spaces
+# str.replace(' ', '.') - takes two arguments and replaces all instances of first argument with second argument
+# str.find('t') - takes string as argument and returns first index value where string is located
+# str.format(var, var) - takes variables as an argument and includes them in string it is run on by
+    #using {} as placeholders
+    #*** note you can now use keywords so you don't have to focus on placement
 test_title = 'the cat in the hat'
 test_title = test_title.title()
 print(test_title) # prints The Cat In The Hat
@@ -99,8 +107,6 @@ for author in author_names:
 print(author_last_names)
 #prints ['Lorde', 'Mistral', 'Toomer', 'Qi', 'Whitman', 'Silverstein', 'Boullosa', 'Suraiyya', 'Hughes', 'Rich', 'Giovanni']
 
-  
-
 #can split strings with escape characters
 #.split('\n') - splits by line breaks
 #.split('\t') - splits by tabs
@@ -118,9 +124,81 @@ print(spring_storm_lines)
 # prints ['The sky has given over ', 'its bitterness. ', 'Out of the dark change ', 'all day long ', 
 # 'rain falls and falls ', 'as if it would never end. ']
 
+#REMEMBER TO JOIN STRINGS YOU START WITH DELIMETER FIRST " ".JOIN(MY_VAR), TO SPLIT YOU START WITH VAR
+# FIRST MY_VAR.SPLIT(' ') 
 #JOINING STRINGS 'delimiter'.join(list_you_want_to_join)
 # my_munequita = ['My', 'Spanish', 'Harlem', 'Mona', 'Lisa']
 # print(' '.join(my_munequita))
 # => 'My Spanish Harlem Mona Lisa'
 reapers_line_one_words = ["Black", "reapers", "with", "the", "sound", "of", "steel", "on", "stones"]
 reapers_line_one = ' '.join(reapers_line_one_words)
+
+santana_songs = ['Oye Como Va', 'Smooth', 'Black Magic Woman', 'Samba Pa Ti', 'Maria Maria']
+santana_songs_csv = ','.join(santana_songs)
+print(santana_songs_csv)
+# => 'Oye Como Va,Smooth,Black Magic Woman,Samba Pa Ti,Maria Maria'
+#can also join using escape characters 
+smooth_fifth_verse_lines = ['Well I\'m from the barrio', 'You hear my rhythm on your radio']
+smooth_fifth_verse = '\n'.join(smooth_fifth_verse_lines)
+print(smooth_fifth_verse)
+#Well I'm from the barrio
+#You hear my rhythm on your radio
+
+love_maybe_lines = ['Always    ', '     in the middle of our bloodiest battles  ', 'you lay down your arms', '           like flowering mines    ','\n' ,'   to conquer me home.    ']
+love_maybe_lines_stripped = []
+for string in love_maybe_lines: #can't use strip on a list so you have to run a for loop
+  love_maybe_lines_stripped.append(string.strip())
+print(love_maybe_lines_stripped)
+#prints ['Always', 'in the middle of our bloodiest battles', 'you lay down your arms', 'like flowering mines', '', 'to conquer me home.']
+love_maybe_full = '\n'.join(love_maybe_lines_stripped)
+print(love_maybe_full)
+#=> Always
+# in the middle of our bloodiest battles
+# you lay down your arms
+# like flowering mines
+
+# to conquer me home.
+
+god_wills_it_line_one = "The very earth will disown you"
+disown_placement = god_wills_it_line_one.find('disown')
+print(disown_placement) 
+#=> 20
+
+def poem_title_card(title, poet):
+  return 'The poem "{}" is written by {}.'.format(title, poet)
+print(poem_title_card("Endless Love", "Mauschen"))
+# => The poem "Endless Love" is written by Mauschen.
+
+highlighted_poems = "Afterimages:Audre Lorde:1997,  The Shadow:William Carlos Williams:1915, Ecstasy:Gabriela Mistral:1925,   Georgia Dusk:Jean Toomer:1923,   Parting Before Daybreak:An Qi:2014, The Untold Want:Walt Whitman:1871, Mr. Grumpledump's Song:Shel Silverstein:2004, Angel Sound Mexico City:Carmen Boullosa:2013, In Love:Kamala Suraiyya:1965, Dream Variations:Langston Hughes:1994, Dreamwood:Adrienne Rich:1987"
+print(highlighted_poems)
+highlighted_poems_list = highlighted_poems.split(',')
+print(highlighted_poems_list)
+highlighted_poems_stripped = []
+for poem in highlighted_poems_list:
+  highlighted_poems_stripped.append(poem.strip())
+print(highlighted_poems_stripped)
+highlighted_poems_details = []
+for poem in highlighted_poems_stripped:
+  highlighted_poems_details.append(poem.split(':'))
+titles = []
+poets = []
+dates = []
+for item in highlighted_poems_details:
+  titles.append(item[0])
+  poets.append(item[1])
+  dates.append(item[2])
+for i in range(0,len(highlighted_poems_details)):
+    print('The poem {} was published by {} in {}'.format(titles[i], poets[i], dates[i]))
+
+# => The poem Afterimages was published by Audre Lorde in 1997
+# The poem The Shadow was published by William Carlos Williams in 1915
+# The poem Ecstasy was published by Gabriela Mistral in 1925
+# The poem Georgia Dusk was published by Jean Toomer in 1923
+# The poem Parting Before Daybreak was published by An Qi in 2014
+# The poem The Untold Want was published by Walt Whitman in 1871
+# The poem Mr. Grumpledump's Song was published by Shel Silverstein in 2004
+# The poem Angel Sound Mexico City was published by Carmen Boullosa in 2013
+# The poem In Love was published by Kamala Suraiyya in 1965
+# The poem Dream Variations was published by Langston Hughes in 1994
+# The poem Dreamwood was published by Adrienne Rich in 1987
+
