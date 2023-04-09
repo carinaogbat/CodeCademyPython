90c8ddaacf89d532071788add1b50c2327a7522b
Carina Ogbat <carinawrightogbat@gmail.com>
2023-04-08 19:33:58 -0700

    added multiple key notes and overwrite notes

--- dictionaries.py
+++ dictionaries.py
@@ -1,3 +1,15 @@
+###ADD Multiple Keys###
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+user_ids.update({'theLooper': 138475, 'stringQueen': 85739})
+print(user_ids)
+  # prints 
+# {'teraCoder': 9018293, 'proProgrammer': 119238, 'theLooper': 138475, 'stringQueen': 85739}
+
+###Overwrite Values###
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+oscar_winners["Supporting Actress"] = "Viola Davis"
+oscar_winners["Best Picture"] = "Moonlight"
+
 ###DICTIONARY COMPREHENSIONS***
 drinks = ["espresso", "chai", "decaf", "drip"]
 caffeine = [64, 40, 0, 120]
