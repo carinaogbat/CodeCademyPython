b2cfadb3e0d8a468207a699f84a7abc732f4e6ba
Carina Ogbat <carinawrightogbat@gmail.com>
2023-04-13 13:35:37 -0700

    added explanation for with open vs open

--- files.py
+++ files.py
@@ -20,3 +20,33 @@ with open('bad_bands.txt', 'w') as bad_bands_doc:
 #you can also use open to add to a file with the "a" argument
 with open("cool_dogs.txt", "a") as cool_dogs_file:
   cool_dogs_file.write("\nAir Buddy") #this will add Air Buddy to the file as a newline
+
+##using with open() vs open()
+#it's better to open a file using the with block because everything indented inside the block will run and then close 
+# the file when the block is closed. If you open just with open() the file will not close until you write close(). 
+# This can impact performance as files exist outside of your Python script and other programs on your computer 
+# may be trying to access the file.
+
+fun_cities_file = open('fun_cities.txt', 'a')
+ 
+# We can now append a line to "fun_cities".
+fun_cities_file.write("Montréal")
+ 
+# But we need to remember to close the file
+fun_cities_file.close()
+
+with open('fun_file.txt') as close_this_file:
+
+  setup = close_this_file.readline()
+  punchline = close_this_file.readline()
+
+print(setup)
+
+#CSV files are a text file that impose a structure to their data, CSV stands for comma separated value and they 
+# are usually the way that data from spreadsheet software (such as microsoft xl or google sheets) in exported 
+# in a poratable format. The first row of a CSV will usally represent the labels of the data rather than the data.
+with open('logger.csv') as log_csv_file:
+  line = log_csv_file.readlines()
+  print(line)
+
+
