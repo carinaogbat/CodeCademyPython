59aaad2e2ecc05225a431756f92a7e4a19e38e69
Carina Ogbat <carinawrightogbat@gmail.com>
2023-04-14 07:45:33 -0700

    added dictwriter method

--- files.py
+++ files.py
@@ -48,9 +48,6 @@ print(setup)
 with open('logger.csv') as log_csv_file:
   line = log_csv_file.readlines()
   print(line)
-
-
-
 #=> ['Name,Age,ID\n', 
 # 'Richard Andrews,43,0de2ecf31df2386377b1d2dc4fae8b16fad05ad0 
 # \n', 'Hailey Sellers,24,3d9b8a95458c1df2687191e8146a97ca4afb28aa          
@@ -74,5 +71,39 @@ with open('users.csv', newline='') as users_csv: #We pass the additional keyword
   user_reader = csv.DictReader(users_csv)
   for row in user_reader:
     list_of_email_addresses.append(row['Email'])
+
+import csv
+with open('cool_csv.csv') as cool_csv_file:
+  cool_csv_dict = csv.DictReader(cool_csv_file)
+  for row in cool_csv_dict:
+    print(row["Cool Fact"])
+
+#csv files are not just separated by commas, they can have different delimeters besides commas, you just have to specify the delimiter in the DictReader call
+import csv
+with open('books.csv') as books_csv:
+  books_reader = csv.DictReader(books_csv, delimiter="@")
+  isbn_list = []
+  for row in books_reader:
+    isbn_list.append(row["ISBN"])
+
+#you can also write csv files: We import the csv library, and then open a new CSV file in write-mode by passing the 'w' argument to the open() function.
+
+# We then define the fields we’re going to be using into a variable called fields. We then instantiate our CSV writer object and pass two arguments. The first is output_csv, the file handler object. The second is our list of fields fields which we pass to the keyword parameter fieldnames.
+
+# Now that we’ve instantiated our CSV file writer, we can start adding lines to the file itself! First we want the headers, so we call .writeheader() on the writer object. This writes all the fields passed to fieldnames as the first row in our file. Then we iterate through our big_list of data. Each item in big_list is a dictionary with each field in fields as the keys. We call output_writer.writerow() with the item dictionaries which writes each line to the CSV file.
+
+access_log = [{'time': '08:39:37', 'limit': 844404, 'address': '1.227.124.181'}, {'time': '13:13:35', 'limit': 543871, 'address': '198.51.139.193'}, {'time': '19:40:45', 'limit': 3021, 'address': '172.1.254.208'}, {'time': '18:57:16', 'limit': 67031769, 'address': '172.58.247.219'}, {'time': '21:17:13', 'limit': 9083, 'address': '124.144.20.113'}, {'time': '23:34:17', 'limit': 65913, 'address': '203.236.149.220'}, {'time': '13:58:05', 'limit': 1541474, 'address': '192.52.206.76'}, {'time': '10:52:00', 'limit': 11465607, 'address': '104.47.149.93'}, {'time': '14:56:12', 'limit': 109, 'address': '192.31.185.7'}, {'time': '18:56:35', 'limit': 6207, 'address': '2.228.164.197'}]
+fields = ['time', 'address', 'limit']
+
+import csv
+with open("logger.csv", "w") as logger_csv:
+  # fields = ["time", "limit", "address"] I wrote fields but didn't have to because it was already in code above
+  log_writer = csv.DictWriter(logger_csv, fieldnames=fields)
+
+  log_writer.writeheader()
+  for item in access_log:
+    log_writer.writerow(item)
+
+
 
 
