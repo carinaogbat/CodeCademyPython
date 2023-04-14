d27df0d892b84976b4768342d2644365838bc0c7
Carina Ogbat <carinawrightogbat@gmail.com>
2023-04-14 07:16:28 -0700

    added csv dictionary method

--- files.py
+++ files.py
@@ -50,3 +50,29 @@ with open('logger.csv') as log_csv_file:
   print(line)
 
 
+
+#=> ['Name,Age,ID\n', 
+# 'Richard Andrews,43,0de2ecf31df2386377b1d2dc4fae8b16fad05ad0 
+# \n', 'Hailey Sellers,24,3d9b8a95458c1df2687191e8146a97ca4afb28aa          
+# \n', 'Jessica Pace,39,a5daa63ef893cb86bc8df1110cc9a5f8e1d0c563            
+# \n', 'Jasmine Escobar,42,9844e403841ec84b9a2fb3caf9d2a1c9ee042d31         
+# \n', 'Karen Kelly,26,8338f76ac0e9a76d73d57790f1d9843b185b5428             
+# \n', 'Patricia Christensen,70,23099bb630c1c64989458393045f08de3bac0eb9    
+# \n', 'Jessica Hansen,24,a8c035ccd099ef909a46e0d96b76c0f132c9c562          
+# \n', 'Raymond Adams,53,a051901830ff6c2095524ef92b1541eef9f8c64d           
+# \n', 'Stephanie Morrow,53,3bad04a5fc0a7ec33735ae45535f354887988f35        
+# \n', 'Timothy Ramos,34,b4930920b5256c4e592541297e43a556c8fe33a8 \n', '\n']
+
+#Python has another option for reading csv files, convertin the data into a dictionary using the csv library’s 
+# DictReader object. Here’s how we’d create a list of the email addresses of all of the users in the above table:
+import csv
+ 
+list_of_email_addresses = []
+with open('users.csv', newline='') as users_csv: #We pass the additional keyword argument newline='' to the file opening 
+                                                #open() function so that we don’t accidentally mistake a line break in 
+                                                # one of our data fields as a new row in our CSV 
+  user_reader = csv.DictReader(users_csv)
+  for row in user_reader:
+    list_of_email_addresses.append(row['Email'])
+
+
