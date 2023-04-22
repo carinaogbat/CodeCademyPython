4c96d1b4485f9f0bbc7a1660509fb182eb4da07d
Carina Ogbat <carinawrightogbat@gmail.com>
2023-04-22 15:44:17 -0700

    adding to project with franchises

--- basta_fazoolin_project.py
+++ basta_fazoolin_project.py
@@ -9,23 +9,46 @@ class Menu:
   def __repr__(self):
     return f"Menu: {self.name}, available from: {self.start_time}-{self.end_time}"
 
-    # def calculate_bill(self, purchased_items):
-    #   total_bill = 0
-    #   for item in purchased_items:
-    #     if item in self.items:
-    #       total_bill +=  
+  def calculate_bill(self, purchased_items):
+    total_bill = 0
+    for item in purchased_items:
+      if item in self.items:
+        total_bill += self.items[item]
+    return total_bill
 
 
-
-
-brunch = Menu(name="brunch", items={
-  'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50}, start_time=1100, end_time=1600
+brunch = Menu("brunch", {
+  'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50}, 1100, 1600
  )
 
-early_bird = Menu(name="early bird", items={'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,}, start_time=1500, end_time=1800)
+early_bird = Menu("early bird", {'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,}, 1500, 1800)
 
-dinner = Menu(name="dinner", items= {'crostini with eggplant caponata': 13.00, 'caesar salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,}, start_time=1700, end_time=2300)
+dinner = Menu("dinner", {'crostini with eggplant caponata': 13.00, 'caesar salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,}, 1700, 2300)
 
-kids = Menu(name="kids", items={'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00}, start_time=1100, end_time=2100)
+kids = Menu("kids", {'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00}, 1100, 2100)
 
-print(brunch)
+# print(brunch.calculate_bill(['pancakes', 'home fries', 'coffee']))
+# print(early_bird.calculate_bill(['salumeria plate', 'mushroom ravioli (vegan)']))
+
+class Franchise:
+  def __init__(self, address, menus):
+    self.address = address
+    self.menus = menus
+
+  def __repr__(self):
+    return f"Franchise Address: {self.address}"
+
+  def available_menus(self, time):
+    available_menus_list = []
+    for menu in self.menus:
+      if time in range(menu.start_time, menu.end_time+1):
+        available_menus_list.append(menu)
+    return available_menus_list
+
+flagship_store = Franchise("1232 West End Road", [brunch, early_bird, dinner, kids])
+new_installment = Franchise("12 East Mulberry Street", [brunch, early_bird, dinner, kids])
+print(flagship_store)
+print(new_installment)
+print(flagship_store.available_menus(2100))
+
+
