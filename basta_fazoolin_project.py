0e254789b6788213dc96b1f681db043673f793ed
Carina Ogbat <carinawrightogbat@gmail.com>
2023-04-22 16:04:26 -0700

    completed Basta class project

--- basta_fazoolin_project.py
+++ basta_fazoolin_project.py
@@ -1,3 +1,24 @@
+class Business:
+  def __init__(self, name, franchises):
+    self.name = name
+    self.franchises = franchises
+
+class Franchise:
+  def __init__(self, address, menus):
+    self.address = address
+    self.menus = menus
+
+  def __repr__(self):
+    return f"Franchise Address: {self.address}"
+
+  def available_menus(self, time):
+    available_menus_list = []
+    for menu in self.menus:
+      if time in range(menu.start_time, menu.end_time+1):
+        available_menus_list.append(menu)
+    return available_menus_list
+
+
 class Menu:
 
   def __init__(self, name, items, start_time, end_time):
@@ -17,6 +38,7 @@ class Menu:
     return total_bill
 
 
+
 brunch = Menu("brunch", {
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50}, 1100, 1600
  )
@@ -30,25 +52,23 @@ kids = Menu("kids", {'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.
 # print(brunch.calculate_bill(['pancakes', 'home fries', 'coffee']))
 # print(early_bird.calculate_bill(['salumeria plate', 'mushroom ravioli (vegan)']))
 
-class Franchise:
-  def __init__(self, address, menus):
-    self.address = address
-    self.menus = menus
-
-  def __repr__(self):
-    return f"Franchise Address: {self.address}"
-
-  def available_menus(self, time):
-    available_menus_list = []
-    for menu in self.menus:
-      if time in range(menu.start_time, menu.end_time+1):
-        available_menus_list.append(menu)
-    return available_menus_list
-
 flagship_store = Franchise("1232 West End Road", [brunch, early_bird, dinner, kids])
 new_installment = Franchise("12 East Mulberry Street", [brunch, early_bird, dinner, kids])
 print(flagship_store)
 print(new_installment)
 print(flagship_store.available_menus(2100))
+
+#Arepa
+arepas_menu = Menu("Take a' Arepa", {
+  'arepa pabellon': 7.00, 'pernil arepa': 8.50, 'guayanes arepa': 8.00, 'jamon arepa': 7.50
+}, 1000, 2000)
+
+arepas_place = Franchise("189 Fitzgerald Avenue", [arepas_menu])
+
+basta = Business("Basta Fazoolin' with my Heart", [flagship_store, new_installment])
+
+arepa = Business("Take a' Arepa", [arepas_place])
+print(arepa.franchises[0])
+
 
 
