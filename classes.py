515a47db522cc087cd9c4be5abc6b25f2e4501ab
Carina Ogbat <carinawrightogbat@gmail.com>
2023-04-21 19:34:17 -0700

    added object notes

--- classes.py
+++ classes.py
@@ -179,3 +179,120 @@ print(round_room.circumference())
 # 37.68
 # 113.04
 # 35984.4
+
+#Attributes can be added to user-defined objects after instantiation, so it’s possible for an object to have some 
+# attributes that are not explicitly defined in an object’s constructor. We can use the dir() function to investigate
+#  an object’s attributes at runtime. dir() is short for directory and offers an organized presentation of object 
+# attributes. That’s certainly a lot more attributes than we defined! Python automatically adds a number of attributes
+#  to all objects that get created. These internal attributes are usually indicated by double-underscores. But sure 
+# enough, attribute is in that list.
+
+# Do you remember being able to use type() on Python’s native data types? This is because they are also objects in 
+# Python. Their classes are int, float, str, list, and dict. These Python classes have special syntax for their 
+# instantiation, 1, 1.0, "hello", [], and {} specifically. But these instances are still full-blown objects to 
+# Python
+
+#EXAMPLE 
+fun_list = [10, "string", {'abc': True}]
+ 
+type(fun_list)
+# Prints <class 'list'>
+ 
+dir(fun_list)
+# Prints ['__add__', '__class__', [...], 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 
+# 'remove', 'reverse', 'sort']
+# print(dir(5))
+#=>['__abs__', '__add__', '__and__', '__bool__', '__ceil__', '__class__', '__delattr__', '__dir__', '__divmod__',
+#  '__doc__', '__eq__', '__float__', '__floor__', '__floordiv__', '__format__', '__ge__', '__getattribute__', 
+# '__getnewargs__', '__gt__', '__hash__', '__index__', '__init__', '__init_subclass__', '__int__', '__invert__',
+#  '__le__', '__lshift__', '__lt__', '__mod__', '__mul__', '__ne__', '__neg__', '__new__', '__or__', '__pos__', 
+# '__pow__', '__radd__', '__rand__', '__rdivmod__', '__reduce__', '__reduce_ex__', '__repr__', '__rfloordiv__',
+#  '__rlshift__', '__rmod__', '__rmul__', '__ror__', '__round__', '__rpow__', '__rrshift__', '__rshift__', 
+# '__rsub__', '__rtruediv__', '__rxor__', '__setattr__', '__sizeof__', '__str__', '__sub__', '__subclasshook__',
+#  '__truediv__', '__trunc__', '__xor__', 'bit_length', 'conjugate', 'denominator', 'from_bytes', 'imag', 
+# 'numerator', 'real', 'to_bytes']
+def this_function_is_an_object():
+  pass
+
+print(dir(this_function_is_an_object))
+#=> ['__annotations__', '__call__', '__class__', '__closure__', '__code__', '__defaults__', '__delattr__', 
+# '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__get__', '__getattribute__', '__globals__',
+#  '__gt__', '__hash__', '__init__', '__init_subclass__', '__kwdefaults__', '__le__', '__lt__', '__module__',
+#  '__name__', '__ne__', '__new__', '__qualname__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__',
+#  '__sizeof__', '__str__', '__subclasshook__']
+
+#We learned about the dunder method __init__(). Now, we will learn another dunder method called __repr__(). This is a method we can use to tell Python what we want the string representation of the class to be. __repr__() can only have one parameter, self, and must return a string.
+class Circle:
+  pi = 3.14
+  
+  def __init__(self, diameter):
+    self.radius = diameter / 2
+  
+  # def __repr__(self):
+  #   return f"Circle with radius {self.radius}"
+  
+  def area(self):
+    return self.pi * self.radius ** 2
+  
+  def circumference(self):
+    return self.pi * 2 * self.radius
+  
+  
+medium_pizza = Circle(12)
+teaching_table = Circle(36)
+round_room = Circle(11460)
+print(medium_pizza)
+print(teaching_table)
+print(round_room)
+#WITHOUT REPR
+#=><__main__.Circle object at 0x7fe008b000f0>
+# <__main__.Circle object at 0x7fe008b00080>
+# <__main__.Circle object at 0x7fe008a9cc50>
+
+#WITH REPR
+#=> Circle with radius 6.0
+# Circle with radius 18.0
+# Circle with radius 5730.0
+
+#REVIEW -- NOT COMPLETE WITH SUGGESTIONS
+#you create a constructor by using the __init__ method
+class Student:
+  def __init__(self, name, year):
+    self.name = name
+    self.year = year
+    self.grades = []
+  
+  def add_grade(self, grade):
+    if type(grade) is Grade:
+      self.grades.append(grade)
+    else:
+      return
+  def get_average(self):
+    sum_of_grades = 0
+    for grd in self.grades.score:
+      sum_of_grades += grd
+    number_of_grades = len(self.grades)
+    return sum_of_grades / number_of_grades
+
+roger = Student("Roger van der Weyden", 10)
+sandro = Student("Sandro Botticelli", 12)
+pieter = Student("Pieter Bruegel the Elder", 8)
+
+class Grade:
+  def __init__(self, score):
+    self.score = score
+  minimum_passing = 65
+  def is_passing(self):
+    if self.score > 65:
+      return "this is a passing grade"
+    else:
+      return "this is not a passing grade"
+
+
+new_grade = Grade(100)
+bad_grade = Grade(63)
+pieter.add_grade(new_grade)
+new_grade.is_passing()
+print(new_grade.is_passing())
+print(bad_grade.is_passing())
+pieter.get_average()
