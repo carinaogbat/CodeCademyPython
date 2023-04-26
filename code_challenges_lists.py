b8c5f65886921ff3af4c8a2872c2c6491f3902bc
Carina Ogbat <carinawrightogbat@gmail.com>
2023-04-26 10:23:30 -0700

    adding coding challenges

--- code_challenges_lists.py
+++ code_challenges_lists.py
@@ -72,3 +72,23 @@ def double_index(my_list, index):
   #  Adds the rest of the original list
   my_new_list = my_new_list + my_list[index+1:]
   return my_new_list
+
+# Create a function called middle_element that has one parameter named my_list.
+
+# If there are an odd number of elements in my_list, the function should return the
+#  middle element. If there are an even number of elements, the function should return 
+#  the average of the middle two elements.
+#Write your function here
+def middle_element(my_list):
+  if len(my_list) %2 == 0:
+    sum = my_list[int(len(my_list)/2)] + my_list[int(len(my_list)/2)-1]
+    return sum / 2
+    
+  else:
+    middle_element = int(len(my_list) /2)
+    return my_list[middle_element]
+
+
+
+#Uncomment the line below when your function is done
+print(middle_element([5, 2, -10, -4, 4, 5]))
