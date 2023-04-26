c646884aa6ef0df304ae11dd28e26ab4527111e3
Carina Ogbat <carinawrightogbat@gmail.com>
2023-04-26 10:27:55 -0700

    added larger sum function

--- code_challenges_loops_advanced.py
+++ code_challenges_loops_advanced.py
@@ -3,3 +3,35 @@
 
 # The function should return the list whose elements sum to the greater number. 
 # If the sum of the elements of each list are equal, return lst1.
+
+#Write your function here ***MY SOLUTION***
+def larger_sum(lst1, lst2):
+  sum1 = 0
+  sum2 = 0
+  for num in lst1:
+    sum1 += num
+  for num in lst2:
+    sum2 += num
+  
+  if sum1 == sum2:
+    return lst1
+  elif sum1 > sum2:
+    return lst1
+  else:
+    return lst2
+
+#Uncomment the line below when your function is done
+print(larger_sum([1, 9, 5], [2, 3, 7]))
+
+# ***THEIR SOLUTION ***
+def larger_sum(lst1, lst2):
+  sum1 = 0
+  sum2 = 0
+  for number in lst1:
+    sum1 += number
+  for number in lst2:
+    sum2 += number
+  if sum1 >= sum2:
+    return lst1
+  else: 
+    return lst2
