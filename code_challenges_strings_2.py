822fa285bc28e59a74eef7ad3974bca9c1ca00cd
Carina Ogbat <carinawrightogbat@gmail.com>
2023-05-02 11:17:30 -0700

    added substring between letters function

--- code_challenges_strings_2.py
+++ code_challenges_strings_2.py
@@ -51,3 +51,26 @@ print(count_char_x("mississippi", "s"))
 print(count_char_x("mississippi", "m"))
 # should print 1
 
+
+# *** QUESTION ***
+# Write a function named substring_between_letters that takes a string named word, a 
+# single character named start, and another character named end. This function should 
+# return the substring between the first occurrence of start and end in word. If start 
+# or end are not in word, the function should return word.
+# For example, substring_between_letters("apple", "p", "e") should return "pl".
+
+#My Solution:
+# Write your substring_between_letters function here:
+def substring_between_letters(word, start, end):
+  start_index = word.find(start)
+  end_index = word.find(end)
+  if start_index and end_index >=0:
+    return word[start_index+1:end_index]
+  else:
+    return word
+
+# Uncomment these function calls to test your function:
+print(substring_between_letters("apple", "p", "e"))
+# should print "pl"
+print(substring_between_letters("apple", "p", "c"))
+# should print "apple"
