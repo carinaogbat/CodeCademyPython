d14ce7f27748338f80e5da1ef50434f070ed8693
Carina Ogbat <carinawrightogbat@gmail.com>
2023-05-02 15:32:17 -0700

    added check for name function

--- code_challenges_strings_2.py
+++ code_challenges_strings_2.py
@@ -138,3 +138,32 @@ def x_length_words(sentence, x):
       return False
   return True
 
+  #QUESTION
+#   Write a function called check_for_name that takes two strings as parameters
+#    named sentence and name. The function should return True if name appears
+#     in sentence in all lowercase letters, all uppercase letters, or with any 
+#     mix of uppercase and lowercase letters. The function should return False
+#      otherwise.
+#MY SOLUTION
+    # Write your check_for_name function here:
+def check_for_name(sentence, name):
+  name = name.lower()
+  sentence = sentence.lower()
+  if name in sentence:
+    return True
+  return False
+
+
+# Uncomment these function calls to test your  function:
+print(check_for_name("My name is Jamie", "Jamie"))
+# should print True
+print(check_for_name("My name is jamie", "Jamie"))
+# should print True
+print(check_for_name("My name is Samantha", "Jamie"))
+# should print False
+
+#THEIR SOLUTION
+def check_for_name(sentence, name):
+  return name.lower() in sentence.lower()
+
+
