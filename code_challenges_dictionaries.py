c04803025ea1482e59c06a59f6d16cb43cf2ffd4
Carina Ogbat <carinawrightogbat@gmail.com>
2023-05-03 10:14:55 -0700

    added sum even keys function

--- code_challenges_dictionaries.py
+++ code_challenges_dictionaries.py
@@ -20,3 +20,31 @@ def sum_values(my_dictionary):
   for value in my_dictionary.values():
     total += value
   return total
+
+  #QUESTION
+#   Create a function called sum_even_keys that takes a dictionary named my_dictionary, with all 
+#   integer keys and values, as a parameter. This function should return the sum of the values of
+#    all even keys.
+#MY SOLUTION
+
+# Write your sum_even_keys function here:
+def sum_even_keys(my_dictionary):
+  count = 0
+  for key, value in my_dictionary.items():
+    if key %2 == 0:
+      count += value
+  return count
+
+# Uncomment these function calls to test your  function:
+print(sum_even_keys({1:5, 2:2, 3:3}))
+# should print 2
+print(sum_even_keys({10:1, 100:2, 1000:3}))
+# should print 6
+
+#THEIR SOLUTION
+def sum_even_keys(my_dictionary):
+  total = 0
+  for key in my_dictionary.keys():
+    if key%2 == 0:
+      total += my_dictionary[key]
+  return total
