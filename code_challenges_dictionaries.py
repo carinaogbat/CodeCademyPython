e084aa985a08611891a1a61143a68740b1571438
Carina Ogbat <carinawrightogbat@gmail.com>
2023-05-06 12:39:37 -0700

    added count first letter dictionary

--- code_challenges_dictionaries.py
+++ code_challenges_dictionaries.py
@@ -219,3 +219,39 @@ print(unique_values({0:3, 1:1, 4:1, 5:3}))
 print(unique_values({0:3, 1:3, 4:3, 5:3}))
 # should print 1
 #THEIR SOLUTION WAS THE SAME
+
+#Question
+# Create a function named count_first_letter that takes a dictionary named names as a parameter. 
+# names should be a dictionary where the key is a last name and the value is a list of first names.
+#  For example, the dictionary might look like this:
+
+# names = {"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Lannister": ["Jaime", "Cersei",
+#  "Tywin"]}
+# The function should return a new dictionary where each key is the first letter of a last name,
+#  and the value is the number of people whose last name begins with that letter.
+
+# Write your count_first_letter function here:
+def count_first_letter(names):
+  letter_count_dict = {}
+  for key in names:
+    if key[0] not in letter_count_dict:
+      letter_count_dict[key[0]] = 0
+    if key[0] in letter_count_dict:
+      letter_count_dict[key[0]] += len(names[key])
+  return letter_count_dict
+
+# Uncomment these function calls to test your  function:
+print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Lannister": ["Jaime", "Cersei", "Tywin"]}))
+# should print {"S": 4, "L": 3}
+print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Sannister": ["Jaime", "Cersei", "Tywin"]}))
+# should print {"S": 7}
+
+#THEIR SOLUTION
+def count_first_letter(names):
+  letters = {}
+  for key in names:
+    first_letter = key[0]
+    if first_letter not in letters:
+      letters[first_letter] = 0
+    letters[first_letter] += len(names[key])
+  return letters
