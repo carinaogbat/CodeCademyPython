2a15f648409ee9b72cbfa7962af2dbc214467a97
Carina Ogbat <carinawrightogbat@gmail.com>
2023-05-09 11:17:44 -0700

    coded way to change all robot attributes

--- classes_coding_challenges.py
+++ classes_coding_challenges.py
@@ -96,3 +96,45 @@ robot_2 = DriveBot(motor_speed=35, direction=75, sensor_range=25)
 print(robot_2.motor_speed)
 print(robot_2.direction)
 print(robot_2.sensor_range)
+
+#QUESTION 
+#Create a class variable called all_disabled which is set to False. Next, create two more 
+# class variables called latitude and longitude. Set both of those variables to equal -999999.
+#  A third robot has been created below the first two robots. Set the latitude of all of the
+#  robots to -50.0 at once. Additionally, set the longitude of the robots to 50.0 and set
+#  all_disabled to True. You should be able to set those values using three lines of code.
+class DriveBot:
+  # Create the class variables!
+  all_disabled = False
+  latitude = -999999
+  longitude = -999999
+
+  def __init__(self, motor_speed = 0, direction = 180, sensor_range = 10):
+        self.motor_speed = motor_speed
+        self.direction = direction
+        self.sensor_range = sensor_range
+    
+  def control_bot(self, new_speed, new_direction):
+        self.motor_speed = new_speed
+        self.direction = new_direction
+
+  def adjust_sensor(self, new_sensor_range):
+        self.sensor_range = new_sensor_range
+
+robot_1 = DriveBot()
+robot_1.motor_speed = 5
+robot_1.direction = 90
+robot_1.sensor_range = 10
+
+robot_2 = DriveBot(35, 75, 25)
+robot_3 = DriveBot(20, 60, 10)
+
+# Change the latitude, longitude, and all_disabled values for all three robots using only three lines of code!
+#You can create class variables directly in the class without placing them in the constructor. Also, to change the value of the class variable within all objects of the class, access the class variable directly from the class. Here is an example: DriveBot.class_variable = 5.
+DriveBot.all_disabled = True
+DriveBot.latitude = -50.0
+DriveBot.longitude = 50.0
+
+print(robot_1.latitude)
+print(robot_2.longitude)
+print(robot_3.all_disabled)
