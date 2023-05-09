16fba1ea01671bb97e69c0db7b62a663f43b397a
Carina Ogbat <carinawrightogbat@gmail.com>
2023-05-09 16:38:32 -0700

    added same index advanced loop function

--- code_challenges_loops_advanced.py
+++ code_challenges_loops_advanced.py
@@ -56,7 +56,6 @@ def over_nine_thousand(lst):
     return sum
 
 
-
 #Uncomment the line below when your function is done
 print(over_nine_thousand([8000, 900, 120, 5000]))
 
@@ -94,3 +93,24 @@ def max_num(nums):
     if number > maximum:
       maximum = number
   return maximum
+
+#QUESTION
+#Write a function named same_values() that takes two lists of numbers of equal size as
+#  parameters.
+# The function should return a list of the indices where the values were equal in lst1 
+# and lst2.
+# For example, the following code should return [0, 2, 3]
+
+#MY SOLUTION (WITH HELP)
+#Write your function here
+def same_values(lst1, lst2):
+  same_indexes =[]
+  for index in range(len(lst1)):
+    if lst1[index] == lst2[index]:
+      same_indexes.append(index)
+  return same_indexes
+
+
+
+#Uncomment the line below when your function is done
+print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
