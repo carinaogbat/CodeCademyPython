006e381f93e3e621f93a32865b093258bc3b0c66
Carina Ogbat <carinawrightogbat@gmail.com>
2023-05-10 07:40:14 -0700

    added introduction function

--- code_challenges_functions_advanced.py
+++ code_challenges_functions_advanced.py
@@ -37,3 +37,21 @@ print(tip(10, 25))
 # should print 2.5
 print(tip(0, 100))
 # should print 0.0
+
+#QUESTION
+
+# Write a function named introduction() that has two parameters named first_name and last_name.
+
+# The function should return the last_name, followed by a comma, a space, first_name another space,
+#  and finally last_name.
+
+#MY ANSWER (THEIRS WAS CONCATONATION)
+# Write your introduction function here:
+def introduction(first_name, last_name):
+  return f'{last_name}, {first_name} {last_name}'
+
+# Uncomment these function calls to test your introduction function:
+print(introduction("James", "Bond"))
+# should print Bond, James Bond
+print(introduction("Maya", "Angelou"))
+# should print Angelou, Maya Angelou
