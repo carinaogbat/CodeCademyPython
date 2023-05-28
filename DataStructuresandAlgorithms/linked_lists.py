33033e96f9931331c9d0e58d76e7808aed7ef83e
Carina Ogbat <carinawrightogbat@gmail.com>
2023-05-27 18:58:38 -0700

    added doubly linked lists

--- DataStructuresandAlgorithms/linked_lists.py
+++ DataStructuresandAlgorithms/linked_lists.py
@@ -151,3 +151,31 @@ print(test_list.stringify_list())
 middle_node = find_middle(test_list)
 print(middle_node.value)
 #added handwritten linked list notes
+
+###DOUBLY LINKED LIST
+class Node:
+  def __init__(self, value, next_node=None, prev_node=None):
+    self.value = value
+    self.next_node = next_node
+    self.prev_node = prev_node
+    
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+    
+  def get_next_node(self):
+    return self.next_node
+
+  def set_prev_node(self, prev_node):
+    self.prev_node = prev_node
+    
+  def get_prev_node(self):
+    return self.prev_node
+  
+  def get_value(self):
+    return self.value
+    
+
+class DoublyLinkedList:
+  def __init__(self):
+    self.head_node = None
+    self.tail_node = None
