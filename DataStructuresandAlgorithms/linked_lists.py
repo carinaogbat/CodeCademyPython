aa02c2a23602004f9c7dae073c48b4663296aa4f
Carina Ogbat <carinawrightogbat@gmail.com>
2023-05-28 17:44:04 -0700

    added added to head for doubly linked list

--- DataStructuresandAlgorithms/linked_lists.py
+++ DataStructuresandAlgorithms/linked_lists.py
@@ -179,3 +179,14 @@ class DoublyLinkedList:
   def __init__(self):
     self.head_node = None
     self.tail_node = None
+
+  def add_to_head(self, new_value):
+    new_head = Node(new_value)
+    current_head = self.head_node
+    if current_head != None:
+      current_head.set_prev_node(new_head)
+      new_head.set_next_node(current_head)
+    self.head_node = new_head
+    # current_tail = self.tail_node
+    if self.tail_node is None:
+      self.tail_node = new_head
